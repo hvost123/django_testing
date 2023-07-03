b7def24b6ab907808fae5c10b10c36b1b262d0a1
Evgeniy <zinovev.evgenii@mail.ru>
2023-07-03 22:23:59 +0300

    tound1

---
+++ ya_note/notes/tests/test_trial.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+from notes.models import Note
+
+
+class TestNote(TestCase):
+    # Все нужные переменные сохраняем в атрибуты класса.
+    TITLE = 'Заголовок новости'
+    TEXT = 'Тестовый текст'
+
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = Note.objects.create(
+            # При создании объекта обращаемся к константам класса через cls.
+            title=cls.TITLE,
+            text=cls.TEXT,
+        )
+
+    def test_successful_creation(self):
+        note_count = Note.objects.count()
+        self.assertEqual(note_count, 1)
+
+    def test_title(self):
+        # Чтобы проверить равенство с константой -
+        # обращаемся к ней через self, а не через cls:
+        self.assertEqual(self.note.title, self.TITLE) 
