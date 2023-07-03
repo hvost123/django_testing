c05e8bb74450645842a139c3a6cd42fd0a255f48
Evgeniy <zinovev.evgenii@mail.ru>
2023-07-03 22:40:25 +0300

    round2

--- ya_note/notes/tests/test_trial.py
+++ ya_note/notes/tests/test_trial.py
@@ -4,15 +4,12 @@ from notes.models import Note
 
 
 class TestNote(TestCase):
-    # Все нужные переменные сохраняем в атрибуты класса.
     TITLE = 'Заголовок новости'
     TEXT = 'Тестовый текст'
-
 
     @classmethod
     def setUpTestData(cls):
         cls.news = Note.objects.create(
-            # При создании объекта обращаемся к константам класса через cls.
             title=cls.TITLE,
             text=cls.TEXT,
         )
@@ -22,6 +19,4 @@ class TestNote(TestCase):
         self.assertEqual(note_count, 1)
 
     def test_title(self):
-        # Чтобы проверить равенство с константой -
-        # обращаемся к ней через self, а не через cls:
-        self.assertEqual(self.note.title, self.TITLE) 
+        self.assertEqual(self.note.title, self.TITLE)
